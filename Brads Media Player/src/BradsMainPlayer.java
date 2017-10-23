0b9470ca45f86901260cf868b1b58bc9c2b9047c
Brad <bradkharris@gmail.com>
2017-10-23 12:01:17 -0700

    Update BradsMainPlayer.java

--- Brads Media Player/src/BradsMainPlayer.java
+++ Brads Media Player/src/BradsMainPlayer.java
@@ -3,7 +3,7 @@ public class BradsMainPlayer {
 
 	public static void main(String[] args) {
 
-		MediaPlayer bp = new MediaPlayer(); //unkonwn how to get it to not have the variable bp because, without it, it doesn't work
+		MediaPlayer bp = new MediaPlayer(); //...
 
 	}
 
